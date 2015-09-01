72549b9b5548710a2e8d38c656bdf959f9ed733b
Will Wang <442359809@qq.com>
2015-09-01 17:18:32 +0800

    [comments]: add PngCompress2 to support compress pngs in folders [jiraid]:function [version]:0.0.1

--- lib_apk_shrink/compress/ImageOptim.py
+++ lib_apk_shrink/compress/ImageOptim.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+from lib_apk_shrink.utils import shrink_utils
+
 __author__ = 'tiantong'
 import os
 
@@ -13,13 +15,13 @@ class ImageOptim(BaseCompressTool):
     def __init__(self, command):
         self.command = command
 
-    def compress(self, file_string):
+    def compress(self, floder_string):
         assert isinstance(super(ImageOptim, self).compress, object)
-        super(ImageOptim, self).compress(file_string)
-        command = self.command + " " + file_string
-        old_size = os.path.getsize(file_string);
+        super(ImageOptim, self).compress(floder_string)
+        command = self.command + " " + floder_string
+        old_size = shrink_utils.get_path_size(floder_string);
         os.popen(command).read()
-        new_size = os.path.getsize(file_string);
+        new_size = shrink_utils.get_path_size(floder_string);
         chg_size = long(old_size) - long(new_size)
         if chg_size > 0:
             return chg_size;
--- lib_apk_shrink/utils/shrink_utils.py
+++ lib_apk_shrink/utils/shrink_utils.py
@@ -3,6 +3,7 @@
 import os
 import json
 import math
+import shutil
 import time
 import functools
 
@@ -91,3 +92,61 @@ def performance():
         return wrapper
 
     return perf_decorator
+
+
+def get_path_size(strPath):
+    if not os.path.exists(strPath):
+        return 0;
+
+    if os.path.isfile(strPath):
+        return os.path.getsize(strPath);
+
+    nTotalSize = 0;
+    for strRoot, lsDir, lsFiles in os.walk(strPath):
+        # get child directory size
+        for strDir in lsDir:
+            nTotalSize = nTotalSize + get_path_size(os.path.join(strRoot, strDir));
+
+            # for child file size
+        for strFile in lsFiles:
+            nTotalSize = nTotalSize + os.path.getsize(os.path.join(strRoot, strFile));
+
+    return nTotalSize;
+
+
+def forceMergeFlatDir(srcDir, dstDir):
+    if not os.path.exists(dstDir):
+        os.makedirs(dstDir)
+    for item in os.listdir(srcDir):
+        srcFile = os.path.join(srcDir, item)
+        dstFile = os.path.join(dstDir, item)
+        forceCopyFile(srcFile, dstFile)
+
+
+def forceCopyFile(sfile, dfile):
+    if os.path.isfile(sfile):
+        shutil.copy2(sfile, dfile)
+
+
+def isAFlatDir(sDir):
+    for item in os.listdir(sDir):
+        sItem = os.path.join(sDir, item)
+        if os.path.isdir(sItem):
+            return False
+    return True
+
+
+def copyTree(src, dst):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isfile(s):
+            if not os.path.exists(dst):
+                os.makedirs(dst)
+            forceCopyFile(s, d)
+        if os.path.isdir(s):
+            isRecursive = not isAFlatDir(s)
+            if isRecursive:
+                copyTree(s, d)
+            else:
+                forceMergeFlatDir(s, d)
