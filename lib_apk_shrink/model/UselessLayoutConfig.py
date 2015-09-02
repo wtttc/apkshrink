1dcb59767462b5ed03a052b1e2ffc848a9226079
Will Wang <442359809@qq.com>
2015-09-02 16:14:34 +0800

    [comments]: 1. add function to search xml 2. change reg to add “,” 3. add batch delete useless layout function [jiraid]:function [version]:0.0.1

--- lib_apk_shrink/model/UselessLayoutConfig.py
+++ lib_apk_shrink/model/UselessLayoutConfig.py
@@ -5,12 +5,15 @@
 class UselessLayoutConfig(object):
     src_dir = []
     layout_dir = []
+    xml_dir = []
 
     def __init__(self, dict=[]):
         if 'src_dir' in dict:
             self.src_dir = dict['src_dir']
         if 'layout_dir' in dict:
             self.layout_dir = dict['layout_dir']
+        if 'xml_dir' in dict:
+            self.xml_dir = dict['xml_dir']
 
     def __repr__(self):
         return 'UselessLayoutConfig'
